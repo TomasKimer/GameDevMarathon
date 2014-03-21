faf64b69df088dab9b757f030c037dc50a1a2d72
david-sabata <david.sabata@business-factory.cz>
2014-03-21 22:07:16 +0100

    spawn vlny, straight mob

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -3,13 +3,51 @@ using System.Collections;
 
 public class GameController : MonoBehaviour {
 
-	// Use this for initialization
+	private int currentWave;
+
+	
 	void Start () {
-	
+		Reset ();
 	}
-	
-	// Update is called once per frame
+
+
+	void Reset() {
+		currentWave = 0;
+	}
+
+
 	void Update () {
-	
+		if (Input.GetKeyDown(KeyCode.Space)) {
+			SpawnWave(0);
+		}
 	}
+
+
+
+	private void SpawnWave(int wave) {
+		if (wave == 0) {
+			SpawnLineFromLeft();
+		}
+	}
+
+
+
+
+
+
+	private void SpawnLineFromLeft() {
+		GameObject prefab = GameObject.Find ("StraightMob");
+
+		for (int i = -8; i < 10; i += 2) {
+			GameObject obj = (GameObject)Instantiate(prefab);
+			obj.transform.position = new Vector3(-8, 1, i);
+
+			StraightMob mob = (StraightMob)obj.GetComponent("StraightMob");
+			mob.speed = 5;
+			mob.SetDir(new Vector3(1, 0, 0));
+		}
+	}
+
+
+
 }
