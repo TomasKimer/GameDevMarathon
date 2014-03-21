4bcbbbdc949bce28130fd7bf22ef0021d464f95b
david-sabata <david.sabata@business-factory.cz>
2014-03-21 23:31:25 +0100

    zmena areny; camera controller

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour {
+
+	public PlayerController player;
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
