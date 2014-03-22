d517d3fce6f09fdc25308e4c8c90739a68c9e539
TomasKimer <tomas.kimer@gmail.com>
2014-03-22 01:11:54 +0100

    Follow camera.

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -5,6 +5,17 @@ public class CameraController : MonoBehaviour {
 
 	public PlayerController player;
 
+	public int leftX = -27; 
+	public int rightX = 13;
+	public int topZ = 8;
+	public int bottomZ = -29;
+
+	public float minRotationVertical = 67.0f;
+	public float maxRotationVertical = 87.0f;
+
+	public float minRotationHorizontal = -10.0f;
+	public float maxRotationHorizontal = 10.0f;
+
 
 	// Use this for initialization
 	void Start () {
@@ -13,6 +24,26 @@ public class CameraController : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-	
+		camera.transform.position = new Vector3(player.transform.position.x, camera.transform.position.y, player.transform.position.z);
+
+		// x - up/down
+		// y - left/right
+
+//		Vector3 rot = camera.transform.rotation.eulerAngles;
+
+		//float OldRange = (topZ - bottomZ);
+		//float NewRange = (1.0f - 0.0f);  
+		//float NewValue = (((camera.transform.position.x - bottomZ) * NewRange) / OldRange) + 0.0f;
+
+		//float vertAmount = NewValue;
+		//float horizAmount = 0.5f;
+		//rot.y = Mathf.Lerp(minRotationHorizontal, maxRotationHorizontal, vertAmount);
+		//rot.y = Mathf.Lerp(minRotationHorizontal, maxRotationHorizontal, horizAmount);
+
+
+		//rot.z += 0.1f;
+
+		//camera.transform.rotation = Quaternion.Euler(rot);
+		//camera.transform.Rotate (new Vector3(0, -0.02f, 0));
 	}
 }
