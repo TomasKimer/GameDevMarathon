ff6745ddb93776557fb0b3b34a0ca484a8ac6d50
unknown <tomas.holomek@BRNNB54.cdc.lan>
2014-03-22 13:50:58 +0100

    MineMob

---
+++ Assets/Scripts/MineMob.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MineMob : BaseMob {
+
+	protected override void UpdateVelocity () {
+		gameObject.transform.Rotate (new Vector3 (0, yAngle, 0) * Time.deltaTime);
+	}
+	
+	protected override void OnCollisionWithWalls(Collision collision) {
+		//Vector3 globalVector = gameObject.transform.TransformDirection(velocity);
+		//globalVector = Vector3.Reflect(globalVector, collision.contacts[0].normal);
+		//velocity = gameObject.transform.InverseTransformDirection(globalVector);
+		//yAngle = Random.Range(-200,200);
+	}
+}
